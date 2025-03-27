1cb115103a049eab4d7ae0bb5b5512801c4d48ea
Oscar <albertlund121@gmail.com>
2025-03-27 12:01:04 +0100

    global in styles

--- src/app/page.js
+++ src/app/page.js
@@ -8,11 +8,11 @@ export default function Home() {
   return (
     <div>
       <div className="icon-row">
-      <IoCloseOutline style={{ display: "flex", alignSelf: "flex-end" }}/>
-        <LiaCookieBiteSolid style={{ fontSize: "56px", transform: "rotate(-90deg)", color: "white"}} />
+        <IoCloseOutline style={{ display: "flex", alignSelf: "flex-end" }} />
+        <LiaCookieBiteSolid style={{ fontSize: "56px", transform: "rotate(-90deg)", color: "white" }} />
       </div>
-        <p>We use Cookies to improve your user experience</p>
-        <LikeButton label="I like Cookies" />
+      <p>We use Cookies to improve your user experience</p>
+      <LikeButton label="I like Cookies" />
     </div>
   );
 }
