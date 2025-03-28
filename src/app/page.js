d9ad45ba7bda58735df68e38c15fa5f94b4c25f4
Oscar <albertlund121@gmail.com>
2025-03-28 13:26:07 +0100

    something

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,26 @@
+'use client'
 import LikeButton from "@/components/LikeButton.jsx"
 import { LiaCookieBiteSolid } from "react-icons/lia";
 import { IoCloseOutline } from "react-icons/io5";
+import { useState } from "react";
 
 
 
 export default function Home() {
+  const [cookiesAccepted, setCookieAccepted] = useState(false);
+  console.log(cookiesAccepted);
   return (
-    <div>
-      <div className="icon-row">
-        <IoCloseOutline style={{ display: "flex", alignSelf: "flex-end" }} />
-        <LiaCookieBiteSolid style={{ fontSize: "56px", transform: "rotate(-90deg)", color: "white" }} />
+    !cookiesAccepted && (
+      <div>
+        <div className="icon-row">
+          <IoCloseOutline style={{ display: "flex", alignSelf: "flex-end" }} />
+          <LiaCookieBiteSolid style={{ fontSize: "56px", transform: "rotate(-90deg)", color: "white" }} />
+        </div>
+        <p>We use Cookies to improve your user experience</p>
+        <button onClick={() => setCookieAccepted(!cookiesAccepted)}>
+          I like cookies
+        </button>
       </div>
-      <p>We use Cookies to improve your user experience</p>
-      <LikeButton label="I like Cookies" />
-    </div>
+    )
   );
 }
--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,17 +1,22 @@
-"use client"
+"use client";
 import { useState } from "react";
 
 import { FaAccessibleIcon } from "react-icons/fa";
 
 const LikeButton = (props) => {
-  const [likes, setLikes] = useState(0);
-
+  /*  const [likes, setLikes] = useState(0); */
+  const [cookiesAccepted, setCookieAccepted] = useState(true);
+  console.log(cookiesAccepted);
   return (
- /*    <button onClick={() => setLikes(likes + 1)}>
+    /*    <button onClick={() => setLikes(likes + 1)}>
       <FaAccessibleIcon /> {props.label}{likes > 1 ? "s" : ""} {likes}
     </button> */
-    <button>{props.label}</button>
-  )
+    !cookiesAccepted && (
+      <button onClick={() => setCookieAccepted(!cookiesAccepted)}>
+        {props.label}
+      </button>
+    )
+  );
 };
 
 export default LikeButton;
